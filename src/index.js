f6cd358f1452327e23c04f8ce975c858683b583e
Sebastian Acosta <jaggersaa.96@hotmail.com>
2020-07-27 21:09:13 -0500

    ...

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ const handlerSession = session({
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(helmet());
 app.use(cors(corsOpts));
+app.use(helmet());
 app.use(morgan('common'));
 app.use(handlerSession);
 
