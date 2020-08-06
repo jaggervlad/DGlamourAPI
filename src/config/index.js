2ec5ba317b966816af1c52e840ff9b720d5b9942
Sebastian Acosta <jaggersaa.96@hotmail.com>
2020-08-06 04:14:07 -0500

    Preparando pa produccion

--- src/config/index.js
+++ src/config/index.js
@@ -4,18 +4,13 @@ const {
   PORT = 4000,
 
   // JWT
-  JWT_SECRET = '27b0a742e2112f067faee7959d5b5ca0aacd991ce8599cb0165ca0044024212e',
-
-  // REDIS
-  REDIS_HOST = 'redis-17801.c17.us-east-1-4.ec2.cloud.redislabs.com',
-  REDIS_PORT = 17801,
-  REDIS_PASSWORD = '2vNxtuPLJT2V1nsZe2MJDStBk0oMyUM9',
+  JWT_SECRET = 'secret',
 
   //DB
-  DB_USERNAME = 'administrador',
-  DB_PASSWORD = 'C58dIq9PihZ8wixj',
-  DB_HOST = 'cluster0.mvoxt.mongodb.net',
-  DB_NAME = 'dglamourapi',
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME,
 } = process.env;
 
 const IN_PROD = NODE_ENV === 'production';
@@ -31,12 +26,6 @@ module.exports.DB_OPTIONS = {
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
-};
-
-module.exports.REDIS_OPTIONS = {
-  host: REDIS_HOST,
-  port: +REDIS_PORT,
-  password: REDIS_PASSWORD,
 };
 
 module.exports.APOLLO_OPTIONS = {
